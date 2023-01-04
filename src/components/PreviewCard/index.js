5100504756e70ab73be2b726dc92cdc5029ee0cd
Michał Grochalski <michal.grochalski@gmail.com>
2023-01-04 13:55:10 +0100

    Add product preview card

--- src/components/PreviewCard/index.js
+++ src/components/PreviewCard/index.js
@@ -1,18 +1,21 @@
-import './PreviewCard.scss';
+import styles from './PreviewCard.module.scss';
+import productImg from '../../images/image-product-desktop.jpg';
+import shoppingCardImg from '../../images/icon-cart.svg'
 
 const PreviewCard = () => (
-    <div>
-        Preview
-
-        Gabrielle Essence Eau De Parfum
-
-        A floral, solar and voluptuous interpretation composed by Olivier Polge,
-        Perfumer-Creator for the House of CHANEL.
-
-        $149.99
-        $169.99
-
-        Add to Cart
+    <div className={styles.card}>
+        <img className={styles.img} src={productImg} alt="product"/>
+        <div className={styles.informationBox}>
+            <p className={styles.category}>Perfume</p>
+            <p className={styles.title}>Gabrielle Essence Eau De Parfum</p>
+            <p className={styles.description}>A floral, solar and voluptuous interpretation composed by Olivier Polge,
+                Perfumer-Creator for the House of CHANEL. </p>
+            <div className={styles.price}>
+                <p className={styles.price__regular}>$149.99</p>
+                <p className={styles.price__discount}>$169.99</p>
+            </div>
+            <button className={styles.btn}><img className={styles.btn__img} src={shoppingCardImg} alt='cart img'/><p>Add to Cart</p></button>
+        </div>
     </div>
 )
 
